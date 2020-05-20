5ace03dfd910029d61208695b082672de806e157
Helena Ferreira <44232337+MHelena45@users.noreply.github.com>
2020-05-20 09:49:21 +0100

    small change

--- src/project/protocols/DeleteProtocol.java
+++ src/project/protocols/DeleteProtocol.java
@@ -18,28 +18,36 @@ public class DeleteProtocol {
     // --------------------   peer initiator
     public static void sendDelete(String file_id) {
 
+        //get file_name
         String file_name = FilesListing.getInstance().getFileName(file_id);
-
         int number = FilesListing.getInstance().get_number_of_chunks(file_name);
 
         // Remove entry with the file_name and correspond file_id from allFiles
         FilesListing.getInstance().delete_file_records(file_name, file_id); //no reason to keep them
 
-        DeleteMessage deleteMessage = new DeleteMessage(file_id);
+        DeleteMessage deleteMessage = new DeleteMessage(ChordNode.this_node.key, file_id);
         Runnable task = () -> processDelete(deleteMessage, number,0);
         Peer.scheduled_executor.execute(task);
     }
 
+    /**
+     * sends delete message to all peers register as peers who backup the file
+     * @param message delete message
+     * @param number_chunks number of chunks that the file have
+     * @param tries current number of files
+     */
     public static void processDelete(DeleteMessage message, int number_chunks, int tries) {
         String file_id = message.getFileId();
+
         if(tries >= 10){
             System.out.println("Couldn't delete all chunks of the file " + file_id);
+            //keeps track of the un-deleted files
             Store.getInstance().changeFromBackupToDelete(file_id);
             return;
         }
 
         if (Store.getInstance().checkIfAllDeleted(file_id)) {
-            System.out.println("Delete all chunks of the file " + file_id);
+            System.out.println("All chunks of the file " + file_id + " were deleted");
 
             for(int i = 0; i < number_chunks; i++ ) {
                 String chunk_id = file_id + "_" + i;
@@ -52,8 +60,7 @@ public class DeleteProtocol {
             String chunk_id = file_id + "_" + i;
             ArrayList<BigInteger> keys = Store.getInstance().get_backup_chunks_occurrences(chunk_id);
             for(int j= 0; j < keys.size(); j++) {
-                NodeInfo nodeInfo = ChordNode.closestPrecedingNode(keys.get(j));
-                message.setSender(nodeInfo.key);
+                NodeInfo nodeInfo = ChordNode.findPredecessor(keys.get(j));
                 try {
                     ChordNode.makeRequest(message, nodeInfo.address, nodeInfo.port);
                 } catch (IOException | ClassNotFoundException e) {
@@ -116,10 +123,10 @@ public class DeleteProtocol {
     public static void sendDelete(DeleteMessage deleteMessage) {
         String file_id = deleteMessage.getFileId();
         int number_chunks = Store.getInstance().number_of_chunks(file_id);
+        deleteMessage.setSender(ChordNode.this_node.key);
 
         for(int i = 0; i < number_chunks; i++) {
             NodeInfo nodeInfo = ChordNode.findSuccessor(ChordNode.this_node.key);
-            deleteMessage.setSender(nodeInfo.key);
             try {
                 ChordNode.makeRequest(deleteMessage, nodeInfo.address, nodeInfo.port);
             } catch (IOException | ClassNotFoundException e) {
