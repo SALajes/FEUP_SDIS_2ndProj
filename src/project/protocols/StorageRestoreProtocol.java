558e9e03560c256753d99b14d31e3dbb91fcd3c0
Joao <up201705312@fe.up.pt>
2020-05-29 15:38:23 +0100

    attempt at fixing

--- src/project/protocols/StorageRestoreProtocol.java
+++ src/project/protocols/StorageRestoreProtocol.java
@@ -95,13 +95,11 @@ public class StorageRestoreProtocol {
 
                     if(actual_rep_degree < rep_degree ) {
                         Chunk chunk = FileManager.retrieveChunk(file_id, chunk_number);
-                        if (chunk != null) {
                             PutChunkMessage putchunk = new PutChunkMessage(ChordNode.this_node.key, file_id, chunk_number, rep_degree, chunk.content);
 
                             //done chunk by chunk
                             Runnable task = () -> BackupProtocol.sendPutchunk(putchunk, rep_degree + 1, 0);
                             Peer.thread_executor.execute(task);
-                        }
                     }
                 }
 
